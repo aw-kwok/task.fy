ce21d246444e08e3c24694e2850f7cfad6d7831b
Andrew Kwok <67013831+aw-kwok@users.noreply.github.com>
2024-01-01 13:42:39 -0500

    Began to integrate Google Sign-in

--- task_fy/api/serializers.py
+++ task_fy/api/serializers.py
@@ -5,3 +5,8 @@ class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ('user_id', 'first_name', 'last_name', 'canvas_id', 'canvas_secret', 'university_id')
+
+class CreateUserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ('user_id', 'first_name', 'last_name')
--- task_fy/api/views.py
+++ task_fy/api/views.py
@@ -1,10 +1,28 @@
 from django.shortcuts import render
-from rest_framework import generics
-from .serializers import UserSerializer
+from rest_framework import generics, status
+from .serializers import UserSerializer, CreateUserSerializer
 from .models import User
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
 # Create your views here.
 
 class UserView(generics.ListAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
+
+class CreateUserView(APIView):
+    serializer_class = CreateUserSerializer
+
+    def post(self, request, format = None):
+        # if current user does not have active session, create
+        if not self.request.session.exists(self.request.session.session_key):
+            self.request.session.create()
+
+        serializer = self.serializer_class(data = request.data)
+        
+        # if fields in data sent in post request are valid
+        if serializer.is_valid():
+            user_id = serializer.data.user_id
+            first_name = serializer.data.first_name
+            last_name = serializer.data.last_name
