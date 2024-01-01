35b0c7655d219da9fdbee6253eea1d5326489ca5
Andrew Kwok <67013831+aw-kwok@users.noreply.github.com>
2023-12-31 22:27:27 -0500

    Removed unused apps

--- task_fy/task_fy/urls.py
+++ task_fy/task_fy/urls.py
@@ -19,7 +19,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('dashboard/', include("dashboard.urls")),
     path('api/', include('api.urls')),
     path('', include('frontend.urls'))
 ]
