281b9328294c588641f1fea79063bf9be7d0e890
Andrew Kwok <67013831+aw-kwok@users.noreply.github.com>
2024-01-07 02:05:03 -0500

    Began implementing the dashboard page, added basic calendar functionality

--- frontend/src/components/ProtectedRoute.jsx
+++ frontend/src/components/ProtectedRoute.jsx
@@ -7,9 +7,9 @@ const ProtectedRoute = () => {
   // make sure to see if id is in cookies
   const isAuthenticated = Cookies.get('id_token')
 
-  if (!isAuthenticated) return (
-    <Navigate to="/sign-in" replace />
-  )
+  // if (!isAuthenticated) return (
+  //   <Navigate to="/sign-in" replace />
+  // )
 
   return (
     <Outlet />
--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,18 +1,43 @@
-import React from 'react'
+import React, { useState } from 'react'
+import Calendar from 'react-calendar'
 
-import { GoogleLogin } from '@react-oauth/google'
 
-import Cookies from 'js-cookie'
-
-import { jwtDecode } from 'jwt-decode'
+import { logoWithName } from '../assets'
 
 const Dashboard = () => {
 
+  const [value, setValue] = useState(new Date())
+
+  const onChange = (nextValue) => {
+    setValue(nextValue)
+    console.log(value)
+  }
+
   return (
-    <div className='bg-primary'>
-      <p>default dashboard view</p>
-      <p>{Cookies.get("auth_token")}</p>
-      {/* <p>{ jwtDecode(Cookies.get("auth_token")) }</p> */}
+    <div className='flex flex-row bg-[#FBF6F6] h-screen'>
+      <div className='flex flex-1 flex-col px-10 py-10 bg-[#F3ECEC] font-outfit'>
+        <img src = { logoWithName } className='w-[25%]' alt="task.fy logo" />
+        <div className='flex flex-col pt-6 pb-3'>
+          <h1 className='px-2 font-extralight text-[32px]'>{ `${value.toLocaleString("default", {month: "long"})} ${value.getFullYear()}` }</h1>
+        </div>
+        <div className={`bg-primary rounded-3xl`}>
+          <Calendar
+            className="text-center px-4 py-4 text-[#4C6BDA]"
+            calendarType='gregory'
+            showNavigation = { false }
+            onChange = { onChange }
+            tileClassName={`text-[#000000] text-[14px] font-outfit`}
+            value = { value }
+          />
+        </div>
+        
+      </div>
+      <div className='flex flex-1'>
+
+      </div>
+      <div className='flex flex-1'>
+
+      </div>
     </div>
   )
 }
